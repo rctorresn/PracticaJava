f97646a1e9871d5442a140415537a7f7145a1d7a
rctorresn <rammsest@gmail.com>
2021-06-02 19:02:42 -0500

    semana 5

---
+++ Fundamentos/05-02-SentenciaIfElse-parte2-CFJ/HolaMundoJava/src/HolaMundo.java
@@ -0,0 +1,35 @@
+
+//Mi clase en Java
+public class HolaMundo {
+
+    public static void main(String args[]) {
+        var condicion = true;
+
+        if (condicion) {
+            System.out.println("Condicion verdadera");
+        } else {
+            System.out.println("Condicion falsa");
+        }
+        
+        var numero = 5;
+        var numeroTexto = "Numero desconocido";
+        
+        if( numero == 1 ){
+            numeroTexto = "Numero uno";
+        }
+        else if( numero == 2 ){
+            numeroTexto = "Numero dos";
+        }    
+        else if( numero == 3 ){
+            numeroTexto = "Numero tres";
+        }
+        else if( numero == 4){
+            numeroTexto = "Numero cuatro";
+        }
+        else{
+            numeroTexto = "Numero no encontrado";
+        }
+        
+        System.out.println("numeroTexto = " + numeroTexto);
+    }
+}
